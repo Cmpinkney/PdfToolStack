3859c9d94e4f6607094d6e2eb6cba3bbb1cc83f7
cmpinkney <chrystal.pinkney@gmail.com>
2026-05-21 16:38:16 -0500

    Prevent auth redirect on public pricing page

--- PdfToolStack.Web/Services/OptionalAuthorizationMessageHandler.cs
+++ PdfToolStack.Web/Services/OptionalAuthorizationMessageHandler.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 namespace PdfToolStack.Web.Services
@@ -5,24 +6,31 @@ namespace PdfToolStack.Web.Services
     public sealed class OptionalAuthorizationMessageHandler : DelegatingHandler
     {
         private readonly IAccessTokenProvider _tokenProvider;
+        private readonly AuthenticationStateProvider _authStateProvider;
 
         public OptionalAuthorizationMessageHandler(
-            IAccessTokenProvider tokenProvider)
+            IAccessTokenProvider tokenProvider,
+            AuthenticationStateProvider authStateProvider)
         {
             _tokenProvider = tokenProvider;
+            _authStateProvider = authStateProvider;
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(
             HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
-            var tokenResult = await _tokenProvider.RequestAccessToken();
-            if (tokenResult.TryGetToken(out var token))
+            var authState = await _authStateProvider.GetAuthenticationStateAsync();
+            if (authState.User.Identity?.IsAuthenticated == true)
             {
-                request.Headers.Authorization =
-                    new System.Net.Http.Headers.AuthenticationHeaderValue(
-                        "Bearer",
-                        token.Value);
+                var tokenResult = await _tokenProvider.RequestAccessToken();
+                if (tokenResult.TryGetToken(out var token))
+                {
+                    request.Headers.Authorization =
+                        new System.Net.Http.Headers.AuthenticationHeaderValue(
+                            "Bearer",
+                            token.Value);
+                }
             }
 
             return await base.SendAsync(request, cancellationToken);
